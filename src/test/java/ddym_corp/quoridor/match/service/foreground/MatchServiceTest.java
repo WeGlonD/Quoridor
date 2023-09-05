e178be046fea59a6843dc4273831fd3217eec56c
jaemoyang <didwoah7@gmail.com>
2023-09-05 20:47:09 +0900

    background matchService release

--- src/test/java/ddym_corp/quoridor/match/service/foreground/MatchServiceTest.java
+++ src/test/java/ddym_corp/quoridor/match/service/foreground/MatchServiceTest.java
@@ -3,11 +3,9 @@ package ddym_corp.quoridor.match.service.foreground;
 import ddym_corp.quoridor.match.utils.MatchResponseDto;
 import ddym_corp.quoridor.user.User;
 import ddym_corp.quoridor.user.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
